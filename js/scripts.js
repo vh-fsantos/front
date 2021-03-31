f1092c5df1eea5a2c81507e623c41899601d969e
vh-fsantos <vitor-hugo962@hotmail.com>
2021-03-31 20:28:58 -0300

    Improvement in FAQ Itens arrow animation

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,11 @@ $(function() {
 
 $(".faqDescription").hide();
 
-var counter = 0;
-
 $(".faqHeader").on('click', function() {
-    counter++;
     const description = $(this).parent().find(".faqDescription");
     description.toggle();
 
-    if (counter % 2 != 0) {
+    if (description.is(":visible")) {
         $(this).find(".faqOpen").css({ transform: "rotate(180deg)" });
     } else {
         $(this).find(".faqOpen").css({ transform: "rotate(0deg)" });
