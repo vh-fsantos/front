03fca20d1d256b0f570474f949f05354e2ae85a7
vh-fsantos <vitor-hugo962@hotmail.com>
2021-03-31 20:08:30 -0300

    Improvement Toggle FAQ Itens

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,16 @@ $(function() {
 
 $(".faqDescription").hide();
 
+var counter = 0;
+
 $(".faqHeader").on('click', function() {
+    counter++;
     const description = $(this).parent().find(".faqDescription");
     description.toggle();
 
+    if (counter % 2 != 0) {
+        $(this).find(".faqOpen").css({ transform: "rotate(180deg)" });
+    } else {
+        $(this).find(".faqOpen").css({ transform: "rotate(0deg)" });
+    }
 })
