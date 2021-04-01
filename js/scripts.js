331a1e264acb88d95aad5b9aba336144e89bd220
vh-fsantos <vitor-hugo962@hotmail.com>
2021-03-31 21:39:03 -0300

    Adjust sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 $(function() {
-    $(".faqHeader").on('click', function() {
+    $(".faqHeader").on("click", function() {
         const description = $(this).parent().find(".faqDescription");
-        description.slideToggle();
-
         if (description.is(":visible")) {
-            $(this).find(".faqOpen").css({ transform: "rotate(180deg)" });
-        } else {
             $(this).find(".faqOpen").css({ transform: "rotate(0deg)" });
+        } else {
+            $(this).find(".faqOpen").css({ transform: "rotate(180deg)" });
         }
+        description.slideToggle();
     });
 });
