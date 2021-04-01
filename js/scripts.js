cf35dcda40f054e34748a388034cb8d797831aa5
vh-fsantos <vitor-hugo962@hotmail.com>
2021-04-01 10:55:06 -0300

    Create Script JSON, first try

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $(function() {
+    $.getJSON('duvidas.json', function(data) {
+
+        data.lista_tarefas.map(faq => {
+            $('.faqWrapper').append(`<div class="faqItem" id="${faq.id}">
+                           <div class="faqHeader">
+                               <h2 class="faqTitle">${faq.titulo}</h2>
+                               <div class="faqLine"></div>
+                               <img src="img/seta.png" alt="" class="faqOpen">
+                           </div>
+                           <p class="faqDescription">${faq.descricao}</p>
+                       </div>`)
+        })
+    });
+
     $(".faqHeader").on("click", function() {
         const description = $(this).parent().find(".faqDescription");
         if (description.is(":visible")) {
