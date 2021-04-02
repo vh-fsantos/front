fdc453046178bbf6953a77a4cfec5705b6867806
vh-fsantos <vitor-hugo962@hotmail.com>
2021-04-02 14:00:04 -0300

    Comment code and last commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 $(function() {
-    $("body").on("click", ".menuButton", function() {
+    $("body").on("click", ".menuButton", function() { // Animation of dropdown menu slide right to left
         const dropdown = $(this).parent().find(".containerItems");
         dropdown.toggleClass("active");
     });
 
-    $("body").on("click", ".faqHeader", function() {
+    $("body").on("click", ".faqHeader", function() { // Animation of dropdown FAQ items
         const description = $(this).parent().find(".faqDescription");
         if (description.is(":visible")) {
-            $(this).find(".faqOpen").css({ transform: "rotate(0deg)" });
+            $(this).find(".faqOpen").css({ transform: "rotate(0deg)" }); // Animation of rotate arrows
         } else {
-            $(this).find(".faqOpen").css({ transform: "rotate(180deg)" });
+            $(this).find(".faqOpen").css({ transform: "rotate(180deg)" }); // Animation of rotate arrows
         }
         description.slideToggle();
     });
-    const workAround = [{
+    const workAround = [{ // Workaround to JSON file, if it is not running on webserver
             id: 1,
             titulo: "O catálogo é grátis?",
             descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a ligula risus. Quisque sagittis nunc ut diam sagittis tempor. Etiam mollis, felis a consectetur volutpat,",
@@ -64,9 +64,9 @@ $(function() {
             descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a ligula risus. Quisque sagittis nunc ut diam sagittis tempor. Etiam mollis, felis a consectetur volutpat,",
         },
     ];
-    $.getJSON("duvidas.json", function(data) {
-        data.lista_tarefas.map((faq) => {
-            $(".faqWrapper").append(`<div class="faqItem" id="${faq.id}">
+    $.getJSON("duvidas.json", function(data) { // Reading JSON file, works on webserver
+        data.lista_tarefas.map((faq) => { // Mapping JSON and creating HTML 
+            $(".faqWrapper").append(`<div class="faqItem" id="${faq.id}"> 
             <div class="faqHeader">
                 <h2 class="faqTitle">${faq.titulo}</h2>
                 <div class="faqLine"></div>
@@ -75,11 +75,11 @@ $(function() {
             <p class="faqDescription">${faq.descricao}</p>
         </div>`);
         });
-    }).fail(function() {
+    }).fail(function() { // Error message to workaround
         console.log(
             "GET do JSON falhou por causa do CORS, atualmente trazendo os dados de uma constante."
         );
-        workAround.map((faq) => {
+        workAround.map((faq) => { // Workaround creating HTML
             $(".faqWrapper").append(`<div class="faqItem" id="${faq.id}">
             <div class="faqHeader">
                 <h2 class="faqTitle">${faq.titulo}</h2>
